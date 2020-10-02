dad03444c4bd7b6d7fb390f23fe55f435c5162bd
DEvil0000 <DEvil0000@users.noreply.github.com>
2020-10-02 11:16:46 +0200

    using nanosecond granularity

--- wgettest.sh
+++ wgettest.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 AMOUNT=50
-START=$(date +%s)
+START=$(date +%s.%N)
 wget -q -O /dev/null http://ipv4.download.thinkbroadband.com/50MB.zip
-END=$(date +%s)
-DURATION=$((END - START))
+END=$(date +%s.%N)
+DURATION=$(awk "BEGIN {print $END - $START}")
 RESULT=$(awk "BEGIN {print $AMOUNT/$DURATION}")
 echo $AMOUNT "MB in " $DURATION " seconds."
 echo "That's " $RESULT "MB/s."
